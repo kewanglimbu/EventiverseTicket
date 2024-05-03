e227a33ec4855c04eb3662438c7145fd2319b8a5
kewang <kewang.h.limbu@gmail.com>
2024-05-03 16:50:37 +0545

    Add project files.

---
+++ EventiverseTicket.Admin.Mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EventiverseTicket.Admin.Mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ EventiverseTicket.Admin.Mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EventiverseTicket.Admin.Mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
